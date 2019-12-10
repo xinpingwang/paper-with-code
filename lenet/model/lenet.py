bf59a4ee6b5d0095339bea5aa069a7897fdd7f75
XinPing Wang <xinping.wang@hotmail.com>
2019-12-10 11:28:33 +0800

    support specifying pooling, active and softmax output

--- lenet/model/lenet.py
+++ lenet/model/lenet.py
@@ -7,11 +7,17 @@ from lenet.model.layers import Sampling2d, DropoutConv2d, RBF
 
 class LeNet(nn.Module):
 
-    def __init__(self):
+    def __init__(self, pooling=Sampling2d, active=None, softmax=False):
+        """
+        args:
+            pooling: 指定降采样方式
+            active: 激活函数，默认使用 LeNet 论文中的 tanh
+            softmax: 如果为 True 则最后一层输出为全联接加 softmax 结果，否则为 RBF 输出
+        """
         super(LeNet, self).__init__()
         # kernel
         self.conv1 = nn.Conv2d(1, 6, 5)
-        self.s2 = Sampling2d(6, 2, 2)
+        self.s2 = pooling(6, 2, 2)
         mapping = [[1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1],
                    [1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1],
                    [1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1],
@@ -19,22 +25,32 @@ class LeNet(nn.Module):
                    [0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1],
                    [0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1]]
         self.conv3 = DropoutConv2d(6, 16, mapping, 5)
-        self.s4 = Sampling2d(16, 2, 2)
+        self.s4 = pooling(16, 2, 2)
         self.conv5 = nn.Conv2d(16, 120, 5)
         # an affine operation: y = Wx + b
         self.fc6 = nn.Linear(120, 84)
-        self.rbf = RBF(84, 10, init_weight=RBF_WEIGHT)
+        if softmax:
+            self.fc7 = nn.Linear(84, 10)
+        else:
+            self.rbf = RBF(84, 10, init_weight=RBF_WEIGHT)
+        self.softmax = softmax
+
+        if active is not None:
+            self.active = active
+        else:
+            self.active = lambda x: 1.7259 * (2 * torch.sigmoid(4 / 3 * x) - 1)
 
     def forward(self, x):
-        def active(a):
-            return 1.7259 * (2 * torch.sigmoid(4 / 3 * a) - 1)
-
-        x = active(self.conv1(x))
-        x = active(self.s2(x))
-        x = active(self.conv3(x))
-        x = active(self.s4(x))
-        x = active(self.conv5(x))
+        x = self.active(self.conv1(x))
+        x = self.active(self.s2(x))
+        x = self.active(self.conv3(x))
+        x = self.active(self.s4(x))
+        x = self.active(self.conv5(x))
         x = torch.squeeze(x)
-        x = active(self.fc6(x))
-        x = self.rbf(x)
+        x = self.active(self.fc6(x))
+        if self.softmax:
+            x = self.fc7(x)
+            x = x.softmax(1)
+        else:
+            x = self.rbf(x)
         return x
